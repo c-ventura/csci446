887b5c657957579c829d9b21b2b0f211762f2f4b
Chris Ventura <criscoboom@gmail.com>
2012-01-23 14:35:53 -0700

    Added another print line, learned puts goes to the next line when you use it.

--- project02/exercise1/ex1.rb
+++ project02/exercise1/ex1.rb
@@ -5,3 +5,8 @@ puts "This is fun."
 puts 'Yay! Printing.'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+#added below for extra credit part 1 (and personal testing)
+print "Another Line." 
+print "difference to puts?"
+puts "It adds an end line?"
+puts "Yes it does"
