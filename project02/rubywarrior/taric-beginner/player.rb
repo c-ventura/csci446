8443308a20c1f0e07bd3274ddea3df9c8ba9ad8a
Chris Ventura <criscoboom@gmail.com>
2012-01-23 15:00:14 -0700

    I beat the first floor by walking! I contemplated using walk multiple times in one turn, but it didn't make a difference since there wasn't anything to kill me. On to floor two

--- project02/rubywarrior/taric-beginner/player.rb
+++ project02/rubywarrior/taric-beginner/player.rb
@@ -1,5 +1,9 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+    #I think all I can do is walk, for now. The staircase is 6 spaces away(without overlapping)
+	warrior.walk!
+	#testing to see if I keep walking, of if walking only moves one space.
+	#I can't really tell, since I don't know if play_turn is looping, or if I just keep walking.
+	#I'm sure I'll find out for sure on the second floor.
   end
 end
