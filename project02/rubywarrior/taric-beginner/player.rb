9662d16e0ee7c0be8b2be4f765ea0a84e9a9b2f9
Chris Ventura <criscoboom@gmail.com>
2012-01-23 18:56:56 -0700

    I beat the sixth floor. I added some logic so that at the start, the warrior will just rescue the prisoner. After that, he'll go right unless he's at low health, and under fire from the archers. He is set to heal enough to run in and take out both archers, with some logic set to only make him run away when his health is low while attempting healing. I kind of want to set an if statement so that I don't have to heal before finishing the level, but I know it'd get messy.

--- project02/rubywarrior/taric-beginner/player.rb
+++ project02/rubywarrior/taric-beginner/player.rb
@@ -1,19 +1,39 @@
 class Player
-  def play_turn(warrior)
-    #Floor Five. I can release captives using .rescue!. Whenever a space isn't empty, instead of just attacking, I'll see if it's a captive I can rescue instead.
-	#|@ CaaSC|
-	
-	if warrior.feel.empty? 
-		if warrior.health > 12 #need at least 12 health to kill a big sludge
-			warrior.walk!
-		else
-			(@health_prev > warrior.health ? warrior.walk! : warrior.rest!)
 
+	def initialize #To go all the way to the left, I'm going to use a boolean to mark when I've hit the wall.
+	@left_walled = false
+	end	
+	
+	def play_turn(warrior)
+    #Floor Six. I can now go backwards, to rescue that one captive against the wall.
+	#|C @ S aa|
+	
+		if @left_walled
+			if warrior.feel.empty? 
+				if warrior.health > 18 #need at least 18 health to run in and kill the archers back to back
+					warrior.walk!
+				else
+					(@health_prev > warrior.health ? (warrior.health > 7 ? warrior.walk! : warrior.walk!(:backward)) : warrior.rest!)
+					#I should turn this to normal syntax, it's getting ugly. It basically tells the warrior to run away if he has less than seven health. Otherwise, he'll charge in(or rest).
+				end
+			else #Something's in front of me
+				(warrior.feel.captive? ? warrior.rescue! : warrior.attack!)
+					#if it's a captive, rescue them. Otherwise, attack!
+			end 
+			@health_prev = warrior.health #set what your health was last turn.
+		  
+		else
+			if warrior.feel(:backward).captive?
+				warrior.rescue!:backward
+				@left_walled = true
+			else #Something's behind of me
+			warrior.walk!:backward
+				#if it's a captive, rescue them. Then start going back right.
+			end 
+			@health_prev = warrior.health #set what your health was last turn.
 		end
-	else #Something's in front of me
-		(warrior.feel.captive? ? warrior.rescue! : warrior.attack!)
-			#if it's a captive, rescue them. Otherwise, attack!
-	end 
-	@health_prev = warrior.health #set what your health was last turn.
-  end
+	end
+	
+
+
 end
