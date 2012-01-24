4efa4bb4fa8990822b39ef47b43e029790aa4469
Chris Ventura <criscoboom@gmail.com>
2012-01-23 18:03:50 -0700

    The addition of captives warranted for me to check what was ahead of me instead of blindly attacking it. I checked, and if it was a captive, I would rescue it. If the thing in front of me was anything else, I attacked it. My logic for healing / dealing with archers didn't need to change.

--- project02/rubywarrior/taric-beginner/player.rb
+++ project02/rubywarrior/taric-beginner/player.rb
@@ -1,20 +1,18 @@
 class Player
   def play_turn(warrior)
-    #Floor Four. There are larger sludges, as well as an archer. If I'm not moving, I'll have to make sure I'm not taking damage from the archer
-	#|@ Sa S>|
+    #Floor Five. I can release captives using .rescue!. Whenever a space isn't empty, instead of just attacking, I'll see if it's a captive I can rescue instead.
+	#|@ CaaSC|
 	
 	if warrior.feel.empty? 
 		if warrior.health > 12 #need at least 12 health to kill a big sludge
 			warrior.walk!
 		else
 			(@health_prev > warrior.health ? warrior.walk! : warrior.rest!)
-			#A fancy way to do an if statement. If old health > new health 
-			#(which would occur when healed last turn, but were attacked by an archer)
-			#then start walking to kill em'!
-			#otherwise, just keep healing.
+
 		end
-	else #Since I heal before going in, just attack if something's there.
-			warrior.attack!
+	else #Something's in front of me
+		(warrior.feel.captive? ? warrior.rescue! : warrior.attack!)
+			#if it's a captive, rescue them. Otherwise, attack!
 	end 
 	@health_prev = warrior.health #set what your health was last turn.
   end
