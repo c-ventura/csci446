31c6b9a5e79fac40a1e5c9548bccc41fa0245769
Chris Ventura <criscoboom@gmail.com>
2012-01-23 17:52:55 -0700

    I beat floor four! I had to up my healing threshold to 12. Since they added archers to the level, I added logic so that if I ever started taking fire while healing, I would start moving to attack the archer. I used instance variables '@variable_name' to store my health total from turn to turn. I also found a new way to do if statements. They're not as easy to read, so I'd only use the syntax for simple choices.

--- project02/rubywarrior/taric-beginner/player.rb
+++ project02/rubywarrior/taric-beginner/player.rb
@@ -1,16 +1,21 @@
 class Player
   def play_turn(warrior)
-    #Floor Three. There are 4 sludges, which do 3 damage every attack, so I will have to heal at some point to stop from dying.
+    #Floor Four. There are larger sludges, as well as an archer. If I'm not moving, I'll have to make sure I'm not taking damage from the archer
+	#|@ Sa S>|
 	
-	if warrior.feel.empty? #if the next space is empty and you have enough health to kill a sludge, move into it. Not enough health -> rest. Enemy there -> attack it.
-		if warrior.health > 6
+	if warrior.feel.empty? 
+		if warrior.health > 12 #need at least 12 health to kill a big sludge
 			warrior.walk!
 		else
-			warrior.rest!
+			(@health_prev > warrior.health ? warrior.walk! : warrior.rest!)
+			#A fancy way to do an if statement. If old health > new health 
+			#(which would occur when healed last turn, but were attacked by an archer)
+			#then start walking to kill em'!
+			#otherwise, just keep healing.
 		end
 	else #Since I heal before going in, just attack if something's there.
 			warrior.attack!
 	end 
-
+	@health_prev = warrior.health #set what your health was last turn.
   end
 end
