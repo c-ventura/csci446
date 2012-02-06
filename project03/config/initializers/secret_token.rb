7bfde00de707b85864c2c58096d6113c44e89ad4
Chris Ventura <criscoboom@gmail.com>
2012-02-05 21:10:13 -0700

    I moved my demo into a new folder, preWorkProj3, and turned project03 into my depot app.

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = 'a4a66d68b7da11131f8c6c79b2c9f445ddd5d85380c9b37f0b5e8b2b1b9d4388b2335c91637f83f2baadf0004195d6820676b8e472e1e150efc4a705dc3284ed'
