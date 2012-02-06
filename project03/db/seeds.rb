e1aae974e52757f64dbd74b9be519add3fb82383
Chris Ventura <criscoboom@gmail.com>
2012-02-05 22:58:57 -0700

    I completed through chapter6. Now I have a styled page, with two items in rake seed. No real errors while going through the book.

--- project03/db/seeds.rb
+++ project03/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+#Deleted everything, now it's time for some presets.
+Product.create(title: 'Banana', 
+description: %{<p>Bananas are the fastest growing fruit out there. If you need to get some, go to a store. </p>},
+img_url: 'http://upload.wikimedia.org/wikipedia/commons/f/ff/Banana_and_cross_section.jpg',
+price: 0.55)
+
+#product two
+Product.create(title: 'Pajamas',
+description: %{<p> They're not really running anywhere. </p>},
+img_url: 'http://www.gadgetmadness.com/archives/slanket.gif',
+price: 50.50)
+   
