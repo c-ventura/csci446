4b7074d6d5e210f8892c60f9c783f54d4beb9e56
Chris Ventura <criscoboom@gmail.com>
2012-02-16 06:58:04 -0700

    Finished chapter 12: Added order validation so that all the forms must be filled out. I also added the ability to make a Feed, so that all of the orders can easily be checked from a feed reader. Lastly, I used an addon called will_paginate to list orders more easily on the backpage /orders.

--- project03/app/controllers/products_controller.rb
+++ project03/app/controllers/products_controller.rb
@@ -20,6 +20,13 @@ class ProductsController < ApplicationController
       format.json { render json: @product }
     end
   end
+  
+  def who_bought
+   @product = Product.find(params[:id])
+   respond_to do |format|
+	format.atom
+   end
+  end
 
   # GET /products/new
   # GET /products/new.json
