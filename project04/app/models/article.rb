aa80932f9eadb30e76a313ba9cb10ccd16134fe4
Chris Ventura <criscoboom@gmail.com>
2012-02-21 04:16:07 -0700

    Added validation for creating a new article.

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   after_initialize :default_values
- 
+  validates :title, :author, :body, :creation_date, presence: true
+  validates :edit_count, numericality: {greater_than_or_equal_to: 0.00}
+  validates :title, uniqueness: true
+  
   private
     def default_values
        self.edit_count ||= 0
