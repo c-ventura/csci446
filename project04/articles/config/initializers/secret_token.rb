e490d3eb7baf93087089f955a26f7f9fff28b70e
Chris Ventura <criscoboom@gmail.com>
2012-02-20 20:47:51 -0700

    created my articles doc, now to generate some scaffolding.

---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '6dec12e744ceccf8b27c1e1e84e93765415e113257bc4f7d60ae59ddf914f8dab2837fed8e054147efd20b68f176995f1dc6bdd50c8538f73cde3e6034981dfe'
