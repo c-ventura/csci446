d3a8ebe3c9299341393d2892e281c2295c5e1b4c
Chris Ventura <criscoboom@gmail.com>
2012-03-29 07:58:01 -0600

    Project06 added to start on Gamez, woot.

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project06::Application.config.secret_token = '64ef0283993a0d0fa07b75388a4d99ec6c6313f7e0d145600faa323d3c3922c202afd43d3810f1a83c9c96e4e177cad53fb645dd337a9572fd3951b1f3bf5d8c'
