b5fce184104a8439dd4c01c286637e43690ab233
Chris Ventura <criscoboom@gmail.com>
2012-05-09 18:14:10 -0600

    I realized that this project needs no ruby, so all ruby / rails code was removed from the project.

--- project07/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Project07::Application.config.secret_token = '8ca20db93f993bdd1850f4f62f5412587df1af00d107e74a1f64db8b48dfab22d61181e0f362b30e2cba6fd1562d0251bc94ff94f3613f6564d03b9f4b39bb66'
